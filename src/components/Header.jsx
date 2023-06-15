bb6a66a5d10524506f0c6a8c2923853ab345deae
Abdul Majid Abdalah <abdulmajidabdalah27@gmail.com>
2023-06-15 07:54:47 +0700

    Add New Animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Fade, Slide } from "react-awesome-reveal";
+import { Fade, Zoom } from "react-awesome-reveal";
 
 const Header = () => {
   const [timerDays, setTimerDays] = useState();
@@ -47,60 +47,64 @@ const Header = () => {
           <p className="font-secondary text-center text-2xl pt-4 lg:pt-[70px]">
             The Wedding Of
           </p>
-          <Fade delay={1e3} cascade damping={1e-1} triggerOnce={true}>  
-          <p className="font-mempelai text-center text-3xl lg:text-6xl mt-4 lg:mt-10">
-            Yosep Ramdani & Sri Kartini
-          </p>
-          </Fade>
+          <div className="flex justify-center items-center">
+            <Fade delay={1e3} cascade damping={1e-1} className="font-mempelai text-center text-3xl lg:text-6xl mt-4 lg:mt-10">
+              Yosep Ramdani & Sri Kartini
+            </Fade>
+          </div>
           <p className="font-secondary text-center text-2xl mt-4 lg:mt-5">
             09 Juli 2023
           </p>
         </div>
-        <div className="grid grid-cols-4 gap-4 px-4 lg:px-32 mt-4 lg:mt-[35px]">
-          <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
-            <div className="text-center font-button-cover text-white">
-              <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
-                {timerDays}
-              </p>
-              <p className="text-[18px] leading-[18px]">Hari</p>
+        <Zoom direction="in" fraction={1} delay={1500} triggerOnce={true}>
+          <div className="grid grid-cols-4 gap-4 px-4 lg:px-32 mt-4 lg:mt-[35px]">
+            <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
+              <div className="text-center font-button-cover text-white">
+                <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
+                  {timerDays}
+                </p>
+                <p className="text-[18px] leading-[18px]">Hari</p>
+              </div>
+            </div>
+            <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
+              <div className="text-center font-button-cover text-white">
+                <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
+                  {timerHours}
+                </p>
+                <p className="text-[18px] leading-[18px]">Jam</p>
+              </div>
+            </div>
+            <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
+              <div className="text-center font-button-cover text-white">
+                <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
+                  {timerMinutes}
+                </p>
+                <p className="text-[18px] leading-[18px]">Menit</p>
+              </div>
+            </div>
+            <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
+              <div className="text-center font-button-cover text-white">
+                <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
+                  {timerSeconds}
+                </p>
+                <p className="text-[18px] leading-[18px]">Detik</p>
+              </div>
             </div>
           </div>
-          <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
-            <div className="text-center font-button-cover text-white">
-              <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
-                {timerHours}
-              </p>
-              <p className="text-[18px] leading-[18px]">Jam</p>
-            </div>
-          </div>
-          <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
-            <div className="text-center font-button-cover text-white">
-              <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
-                {timerMinutes}
-              </p>
-              <p className="text-[18px] leading-[18px]">Menit</p>
-            </div>
-          </div>
-          <div className="bg-[#9E795E] flex justify-center items-center h-[80px] lg:h-[103px] rounded-lg">
-            <div className="text-center font-button-cover text-white">
-              <p className="lg:text-[50px] text-[35px] leading-[40px] lg:leading-[65px]">
-                {timerSeconds}
-              </p>
-              <p className="text-[18px] leading-[18px]">Detik</p>
-            </div>
-          </div>
-        </div>
+        </Zoom>
         <article className="lg:px-[5rem] px-4 font-normal font-secondary text-white text-[26px] leading-[30px] mt-4 lg:mt-[58px] mx-2 rounded-lg py-2 backdrop-brightness-50 bg-white/30">
-          <p
-            align="justify"
-            className="text-center lg:text-current text-[22px]"
-          >
-            " Dan di antara tanda-tanda kekuasaan-Nya diciptakan-Nya untukmu
-            pasangan hidup dari jenismu sendiri supaya kamu dapat ketenangan
-            hati dan dijadikannya kasih sayang di antara kamu. Sesungguhnya yang
-            demikian menjadi tanda-tanda kebesaran-Nya bagi orang-orang yang
-            berpikir. "
-          </p>
+          <Zoom direction="in" triggerOnce={true} delay={2000}>
+            <p
+              align="justify"
+              className="text-center lg:text-current text-[22px]"
+            >
+              " Dan di antara tanda-tanda kekuasaan-Nya diciptakan-Nya untukmu
+              pasangan hidup dari jenismu sendiri supaya kamu dapat ketenangan
+              hati dan dijadikannya kasih sayang di antara kamu. Sesungguhnya
+              yang demikian menjadi tanda-tanda kebesaran-Nya bagi orang-orang
+              yang berpikir. "
+            </p>
+          </Zoom>
           <p className="lg:py-[24px] pb-8 pt-2 text-center lg:text-current text-[22px]">
             - Q.S. Ar-Rum: 21 -
           </p>
--- src/components/PasanganMempelai.jsx
+++ src/components/PasanganMempelai.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import wanita from "../assets/sri.jpg";
 import pria from "../assets/yosep.jpg";
-import { Slide, Zoom } from "react-awesome-reveal";
-
+import { Zoom, Fade, Flip } from "react-awesome-reveal";
 
 const PasanganMempelai = () => {
   return (
@@ -12,51 +11,68 @@ const PasanganMempelai = () => {
         id="couple"
       >
         <div className="bg-white rounded-t-[50%]">
-          <Slide triggerOnce={true}
+          <Flip duration={1500}
+            triggerOnce={true}
             className="font-pasangan text-center pt-8 lg:pt-[60px] lg:text-6xl text-4xl"
           >
             Pasangan Mempelai
-          </Slide>
-          <Zoom className="text-center font-secondary lg:text-[26px] text-[22px] leading-relaxed lg:mx-[247px] mx-4 mt-4 lg:mt-[55px]">
+          </Flip>
+          <Zoom
+            direction="in"
+            delay={1500}
+              triggerOnce={true}>
+          <p
+            className="text-center font-secondary lg:text-[26px] text-[22px] leading-relaxed lg:mx-[247px] mx-4 mt-4 lg:mt-[55px]"
+          >
             Tanpa mengurangi rasa hormat, kami bermaksud mengundang
             Bapak/Ibu/Saudara/i untuk menghadiri acara pernikahan kami:
+          </p>
           </Zoom>
         </div>
         <div className="flex justify-center items-center mt-6 bg-white/30 backdrop-brightness-50 py-5 mx-5 rounded-xl">
-          <div>
-            <img
-              src={wanita}
-              alt="wanita"
-              className="lg:h-[250px] lg:w-[250px] w-[200px] h-[200px] mx-auto object-cover rounded-[50%]"
-            />
-            <h1 className="font-mempelai text-center lg:my-2 my-1 text-white">
-              Sri Kartini
-            </h1>
-            <div className="font-secondary text-center lg:text-[26px] text-[22px] leading-relaxed text-white">
-              <p>Putri dari :</p>
-              <p>Bapak Dede</p>
-              <p>&</p>
-              <p>Ibu Anah</p>
+          <Fade direction="left" duration={2000} triggerOnce={true} delay={2000}>
+            <div>
+              <img
+                src={wanita}
+                alt="wanita"
+                className="lg:h-[250px] lg:w-[250px] w-[200px] h-[200px] mx-auto object-cover rounded-[50%]"
+              />
+              <h1 className="font-mempelai text-center lg:my-2 my-1 text-white">
+                Sri Kartini
+              </h1>
+              <div className="font-secondary text-center lg:text-[26px] text-[22px] leading-relaxed text-white">
+                <p>Putri dari :</p>
+                <p>Bapak Dede</p>
+                <p>&</p>
+                <p>Ibu Anah</p>
+              </div>
             </div>
-          </div>
+          </Fade>
         </div>
         <div className="flex justify-center items-center mt-4 lg:mt-8 pb-10 bg-white/30 backdrop-brightness-50 py-5 mx-5 rounded-xl">
-          <div>
-            <img
-              src={pria}
-              alt="pria"
-              className="lg:h-[250px] lg:w-[250px] w-[200px] h-[200px] mx-auto object-cover rounded-[50%]"
-            />
-            <h1 className="font-mempelai text-center my-1 lg:my-2 text-white">
-              Yosep Ramdani
-            </h1>
-            <div className="font-secondary text-center text-[22px] lg:text-[26px] leading-relaxed text-white">
-              <p>Putra dari :</p>
-              <p>Bapak (Alm) H. Anwar Abdussalam</p>
-              <p>&</p>
-              <p>Ibu Hj. Enung Nuriyah</p>
+          <Fade
+            direction="right"
+            duration={2000}
+            delay={1000}
+            triggerOnce={true}
+          >
+            <div>
+              <img
+                src={pria}
+                alt="pria"
+                className="lg:h-[250px] lg:w-[250px] w-[200px] h-[200px] mx-auto object-cover rounded-[50%]"
+              />
+              <h1 className="font-mempelai text-center my-1 lg:my-2 text-white">
+                Yosep Ramdani
+              </h1>
+              <div className="font-secondary text-center text-[22px] lg:text-[26px] leading-relaxed text-white">
+                <p>Putra dari :</p>
+                <p>Bapak (Alm) H. Anwar Abdussalam</p>
+                <p>&</p>
+                <p>Ibu Hj. Enung Nuriyah</p>
+              </div>
             </div>
-          </div>
+          </Fade>
         </div>
       </div>
     </>
